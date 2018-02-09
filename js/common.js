bc950499341b121da5b17ff8b09b186bb22f4165
Anton Fokin <1antonfokin@gmail.com>
2018-02-09 20:49:05 +0200

    adding accordion-carousel

--- js/common.js
+++ js/common.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
-// MY SANDWICH
-//	$(".s-btn").on('click', function(event){
-//		$(this).toggleClass('btn-animation');
-//		$(".my-sandwich").toggleClass("active");
-//		event.stopPropagation()
-//	});
-//	$("body").on('click', function(){
-//		$(".s-btn").removeClass("btn-animation");
-//		$(".my-sandwich").removeClass("active")
-//	});
+//SEARCH
+	$(".btn-search").on("click", function(event){
+		event.preventDefault();
+		var input = $(this).siblings("input")[0];
+		$(input).toggleClass("active-search");
+		event.stopPropagation();
+	});
+	$(".input-search").on("click", function(event){
+		event.stopPropagation();
+	});
+
+// MY SANDWICHES
+	$(".s-btn").on('click', function(event){
+		$(this).toggleClass('btn-animation');
+		$(".bottom-component").toggleClass("active-m-menu");
+		var accountGroup = $(".account-group");
+		if(accountGroup.hasClass("active-m-menu")){
+			$(accountGroup).removeClass("active-m-menu");
+		}
+		event.stopPropagation()
+	});
+	$(".account-info").on("click", function(event){
+		$(".account-group").toggleClass("active-m-menu");
+		var bottomComponent = $(".bottom-component");
+		if(bottomComponent.hasClass("active-m-menu")){
+			$(bottomComponent).removeClass("active-m-menu");
+			$(".s-btn").removeClass('btn-animation');
+		}
+		event.stopPropagation()
+	});
+
+	$("body").on('click', function(){
+		$(".s-btn").removeClass("btn-animation");
+		$(".bottom-component").removeClass("active-m-menu");
+		$(".active-search").removeClass("active-search");
+		$(".account-group").removeClass("active-m-menu")
+	});
 
 
 // SMOOTH SCROLLING
